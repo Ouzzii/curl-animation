796ef6be666b0434ee0dc3a52b5ac88b7a986d7a
Ouzzii <84467058+Ouzzii@users.noreply.github.com>
2024-07-13 23:44:45 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,37 @@
+from flask import Flask, Response
+import time
+import os, json
+app = Flask(__name__)
+
+def frames(dir):
+    with open(f'{dir}/config.json')as f:
+        config = json.load(f)
+    frames = []
+    for i in range(len(os.listdir(dir))-1):
+        with open(f'{dir}/{i+1}.txt', encoding='utf-8')as f:
+            frame = f.read()
+            if str(i+1) in list(config.keys()):
+                for key, value in config[str(i+1)].items():
+                    frame.replace(key, f'\033[38;2;{value[0]};{value[1]};{value[2]}m{key}\033[0m')
+                    frames.append(frame)
+                    print(i+1, 'numaralı frame renklidir')
+            else:
+                frames.append(frame)
+    return frames
+
+
+clear_screen = "\033[2J\033[H"
+
+@app.route('/animation')
+def animation():
+    def generate():
+        while True:
+            for frame in frames('hello-kitty-kiss'):
+                yield clear_screen + frame + '\n'
+                time.sleep(0.3)
+
+    return Response(generate(), content_type='text/plain')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+    #frames('hello-kitty-kiss')
